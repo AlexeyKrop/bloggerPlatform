4a244b22744e0c433262b8a4d5167cb2dbcefb34
EnzoK <kropotin_aleksey@mail.ru>
2022-11-08 11:46:33 +0300

    feat: add style from post

--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -3,27 +3,31 @@ import React, { FC } from 'react';
 import { PostType } from '../../store/reducers/types';
 import { DropDownMenu } from '../DropDownMenu/DropDownMenu';
 
+import s from './Post.module.css';
+
 type PostPropsType = {
   post: PostType;
 };
 export const Post: FC<PostPropsType> = ({ post }) => {
   return (
-    <div>
+    <div className={s.wrapper}>
       <img
+        className={s.mainImage}
         src="https://www.inkling.com/wp-content/uploads/2021/06/SD-default-image.png"
         alt="post"
       />
       <div>
-        <div>
+        <div className={s.contentWrapper}>
           <img
+            className={s.contentImage}
             src="https://www.inkling.com/wp-content/uploads/2021/06/SD-default-image.png"
             alt="post"
           />
-          <div>
-            <p>{post.title}</p>
-            <p>{post.shortDescription}</p>
-            <p>{post.addedAt}</p>
-            <p>{post.content}</p>
+          <div className={s.content}>
+            <p className={s.title}>{post.title}</p>
+            <p className={s.description}>{post.shortDescription}</p>
+            <p className={s.dateTitle}>{new Date(post.addedAt).toLocaleDateString()}</p>
+            <p className={s.contentSubtitle}>{post.content}</p>
           </div>
           <DropDownMenu />
         </div>
--- src/pages/Posts/Posts.tsx
+++ src/pages/Posts/Posts.tsx
@@ -6,6 +6,8 @@ import { useAppSelector } from '../../hooks/useAppSelector/useAppSelector';
 import { setPostsTC } from '../../store/reducers/postsReducer';
 import { selectPosts } from '../../store/selectors/selectPosts/selectPosts';
 
+import s from './Posts.module.css';
+
 export const Posts: FC = () => {
   const dispatch = useAppDispatch();
   const posts = useAppSelector(selectPosts);
@@ -15,12 +17,13 @@ export const Posts: FC = () => {
   }, [dispatch]);
 
   return (
-    <div>
-      {posts.map(post => (
-        <div key={post.id}>
-          <Post post={post} />
-        </div>
-      ))}
+    <div className={s.wrapper}>
+      {posts &&
+        posts.map(post => (
+          <div key={post.id}>
+            <Post post={post} />
+          </div>
+        ))}
     </div>
   );
 };
